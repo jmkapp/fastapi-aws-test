a40553881515f687f6ce6539ff3d2c87f4326b32
jmkapp <github@jkapp.me>
2026-06-05 14:25:25 +0100

    Does seem to largely work

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,5 +3,38 @@ provider "aws" {
 }
 
 resource "aws_cognito_user_pool" "user_pool" {
-  name = "my_user_pool"
+  name = "fastapi-aws-test"
 }
+
+resource "aws_cognito_user_pool_client" "fastapi_client" {
+  name         = "fastapi-client"
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+
+  generate_secret = false
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+  ]
+
+  access_token_validity  = 1   # hours
+  refresh_token_validity = 30  # days
+
+  prevent_user_existence_errors = "ENABLED"
+}
+
+output "user_pool_client_id" {
+  value = aws_cognito_user_pool_client.fastapi_client.id
+}
+
+resource "aws_cognito_user" "test_user" {
+  username         = "fastapi"
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+}
+
+# User is created with FORCE_CHANGE_PASSWORD status so must be changed:
+# aws cognito-idp admin-set-user-password \
+#   --user-pool-id YOUR_POOL_ID \
+#   --username fastapi \
+#   --password Perm1234! \
+#   --permanent
